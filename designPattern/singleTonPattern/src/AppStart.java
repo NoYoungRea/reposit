7a800ee1932d188ada43d40ae9d57cd0da8a8952
NoYoungRea <rea2169@naver.com>
2020-03-15 20:27:07 +0900

    9

---
+++ designPattern/singleTonPattern/src/AppStart.java
@@ -0,0 +1,9 @@
+
+public class AppStart {
+	public static void main(String[]args) {
+		SingleTonPattern m1=new SingleTonPattern();
+		SingleTonPattern m2=new SingleTonPattern();
+		System.out.println(m1);
+		System.out.println(m2);
+	}
+}	
---
+++ designPattern/singleTonPattern/src/SingleTonPattern.java
@@ -0,0 +1,7 @@
+
+public class SingleTonPattern {
+	public SingleTonPattern() {
+		
+	}
+
+}
