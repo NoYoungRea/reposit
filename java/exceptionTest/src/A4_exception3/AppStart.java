86e58fa66a59716dc25a438f92600f89606f8bbe
NoYoungRea <rea2169@naver.com>
2020-03-16 20:31:38 +0900

    14

--- java/exceptionTest/src/A4_exception3/AppStart.java
+++ java/exceptionTest/src/A4_exception3/AppStart.java
@@ -25,7 +25,7 @@ public class AppStart {
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		try {
 			func();
 		}
---
+++ java/exceptionTest/src/A5_exception4/exception4.java
@@ -0,0 +1,24 @@
+package A5_exception4;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class exception4 {
+
+	public static void func() throws Exception{
+		FileInputStream fis=new FileInputStream("data.txt");
+	}
+	public static void read()	{
+		try {
+			func();
+		}
+		catch(IOException e) {
+			System.out.println("file fail");
+		}
+		catch (Exception e) {
+			System.out.println("x");
+		}
+	}
+	public static void main(String[] args) {
+		read();
+	}
+}
