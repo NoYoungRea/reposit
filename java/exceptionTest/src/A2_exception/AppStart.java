74b138029b2137111dbb435036758b93f4191599
NoYoungRea <rea2169@naver.com>
2020-03-16 19:47:43 +0900

    6

--- java/exceptionTest/src/A2_exception/AppStart.java
+++ java/exceptionTest/src/A2_exception/AppStart.java
@@ -5,9 +5,22 @@ import java.io.IOException;
 public class AppStart {
 	public static void main(String[] args) {
 		try {
-			int n = System.in.read();
-		} catch (IOException e) {
+			System.out.print("input:");
+			int n=System.in.read();
+			int a=10/0;
+
+		}
+		catch(IOException e) {
+			System.out.println("read");
 			e.printStackTrace();
+		}
+		catch(ArithmeticException e) {
+			System.out.println("math");
+			e.printStackTrace();
+		}
+		finally {
+			System.out.println("done");
 		}
 	}
 }
+
