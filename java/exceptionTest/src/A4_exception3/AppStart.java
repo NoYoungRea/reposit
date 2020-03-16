1aa4b2a6539285bf1bf846105ce77a2ff9f73a9e
NoYoungRea <rea2169@naver.com>
2020-03-16 20:03:04 +0900

    12

---
+++ java/exceptionTest/src/A4_exception3/AppStart.java
@@ -0,0 +1,31 @@
+package A4_exception3;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class AppStart {
+	public static void func() {
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream("data.txt");
+		}
+
+		catch (IOException e) {
+			System.out.println("x");
+
+		}
+		finally {
+			try {
+
+				fis.close();
+
+			} 
+			catch (Exception e) {
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		func();
+	}
+}
