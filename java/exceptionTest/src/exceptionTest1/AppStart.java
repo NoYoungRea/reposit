321a114a724fe0890f67e48b8423c46171c8c6a9
NoYoungRea <rea2169@naver.com>
2020-03-16 19:04:00 +0900

    2

---
+++ java/exceptionTest/src/exceptionTest1/AppStart.java
@@ -0,0 +1,12 @@
+package exceptionTest1;
+
+public class AppStart {
+
+
+	public static void main(String[] args) throws Exception {
+		int a=10;
+		int b=0;
+		int c=a/b;
+		System.out.println(c);
+	}
+}
