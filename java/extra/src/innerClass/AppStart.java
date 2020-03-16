e0e193df852af4dc8b0023303ae8cc3b2531c2ca
NoYoungRea <rea2169@naver.com>
2020-03-16 12:35:00 +0900

    1

---
+++ java/extra/src/innerClass/AppStart.java
@@ -0,0 +1,22 @@
+package innerClass;
+class Bike implements Car.Runnable{
+	public void run() {
+		
+	}
+	
+}
+public class AppStart {
+	public static void main(String[]args) {
+		Car c=new Car("hundai");
+		c.showInfo();
+		
+		Car c2=new Car("meri");
+		c.showInfo();
+		
+		Car.Engine e=c.new Engine(5000);
+		e.on();
+		
+		//System.out.println(c.Engine.ENGINE_TYPE); error
+
+	}
+}
---
+++ java/extra/src/innerClass/Car.java
@@ -0,0 +1,52 @@
+package innerClass;
+
+public class Car {
+	public final static String TYPE="car1";
+	private String name;
+	
+	//--------------------------------
+	public Car(String name) {
+		this.name=name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	//--------------------------------
+	public void showInfo() {
+		Engine e=new Engine(3000);
+		e.on();
+	}
+
+	interface Runnable{
+		public abstract void run();
+	}
+	
+	class Engine{
+		public static final String Engine_Type="marine";
+		private int displacement;
+		
+		public Engine(int displacement) {
+			this.displacement=displacement;
+		}
+
+		public int getDisplacement() {
+			return displacement;
+		}
+
+		public void setDisplacement(int displacement) {
+			this.displacement = displacement;
+		}
+		
+		public void on() {
+			System.out.println(Car.TYPE);
+			System.out.println(Car.this.name);
+			System.out.println(displacement);
+			System.out.println(Engine.Engine_Type);
+		}
+		
+	}
+	
+}
