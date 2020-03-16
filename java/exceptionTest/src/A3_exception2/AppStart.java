7debe5dd57f2d8bc6c4122fec515edd86fec18fe
NoYoungRea <rea2169@naver.com>
2020-03-16 19:58:53 +0900

    10

---
+++ java/exceptionTest/src/A3_exception2/AppStart.java
@@ -0,0 +1,21 @@
+package A3_exception2;
+
+public class AppStart {
+	public static int func() {
+		int a=10;
+		int b=0;
+		int value=0;
+		
+		try {
+			value=a/b;
+		}
+		catch(ArithmeticException e) {
+			value=-1;
+		}
+		return value;
+	}
+	public static void main(String[] args) {
+		int a=func();
+		System.out.println(a);
+	}
+}
