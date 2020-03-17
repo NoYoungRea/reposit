cb869f4ecbd978db83d15ea0559cbacb7c0e61a0
NoYoungRea <rea2169@naver.com>
2020-03-17 13:43:48 +0900

    4

--- java/extra/src/equalsTest/AppStart.java
+++ java/extra/src/equalsTest/AppStart.java
@@ -5,10 +5,13 @@ public class AppStart {
 		Person p1=new Person("hgd",20);
 		Person p2=new Person("hgd",20);
 		Person p3=p1;;
+		Object p4=new Person("hgd",20);
 		
 		System.out.println(p1==p2);
 		System.out.println(p1==p3);
 		System.out.println(p1.equals(p2));
 		System.out.println(p1.equals(p3));
+		System.out.println(p1.equals(p4));
+		
 	}
 }
