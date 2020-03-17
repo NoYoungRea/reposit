40cef0d6332f16df3bf6560881582daf71f454a3
NoYoungRea <rea2169@naver.com>
2020-03-17 15:50:25 +0900

    14

--- designPattern/Builder/src/Builder/CarBuilder.java
+++ designPattern/Builder/src/Builder/CarBuilder.java
@@ -2,26 +2,34 @@ package Builder;
 
 public class CarBuilder {
 	private Car car;
-	
+	private static CarBuilder builder = null;
+
 	private CarBuilder() {
-		this.car=new Car("sonata","black",2500);
+
 	}
+
 	public static CarBuilder getBuilder() {
-		return new CarBuilder();
+		if (builder == null)
+			builder = new CarBuilder();
+		builder.car = new Car("sonata", "black", 2500);
+		return builder;
 	}
-	
+
 	public CarBuilder setName(String name) {
 		this.car.setName(name);
 		return this;
 	}
+
 	public CarBuilder setColor(String color) {
 		this.car.setColor(color);
 		return this;
 	}
+
 	public CarBuilder setDisp(int disp) {
 		this.car.setDisp(disp);
 		return this;
 	}
+
 	public Car getInstance() {
 		return car;
 	}
