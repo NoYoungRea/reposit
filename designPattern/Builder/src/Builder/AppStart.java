308c239d1b777b5534f08f078bb3c4edeb7072bb
NoYoungRea <rea2169@naver.com>
2020-03-17 15:39:23 +0900

    10

--- designPattern/Builder/src/Builder/AppStart.java
+++ designPattern/Builder/src/Builder/AppStart.java
@@ -3,6 +3,7 @@ package Builder;
 public class AppStart {
 	public static void main(String[]args) {
 		CarBuilder cb=new CarBuilder();
+		cb.setName("benz").setDisp(4500).setColor("white");
 		Car car=cb.getInstance();
 		System.out.println(car.toString());
 	}
