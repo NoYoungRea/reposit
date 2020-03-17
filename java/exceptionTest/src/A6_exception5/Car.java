45740f3570e0d8161e6325e9badac1ec8b549211
NoYoungRea <rea2169@naver.com>
2020-03-17 11:40:18 +0900

    8

--- java/exceptionTest/src/A6_exception5/Car.java
+++ java/exceptionTest/src/A6_exception5/Car.java
@@ -2,29 +2,39 @@ package A6_exception5;
 
 public class Car {
 	private int fuel;
-
+	private boolean engineOn;
 	public Car(int fuel) {
 		this.fuel = fuel;
 	}
 	public void setFuel(int fuel) {
 		this.fuel=fuel;
 	}
+	public void setEngineOn(boolean engineOn)
+	{
+		this.engineOn=engineOn;
+	}
 	public void run(int dist) {
-		if((dist/10)<=fuel) {
+		
+		try {
+			if(engineOn==false)
+				throw new EngineStopException();
+			if(fuel<dist/10)
+				throw new NotEnoughFuelException();
 			System.out.println(dist+"km");
 			fuel-=(dist/10);
-			System.out.println("fuel"+fuel+"left");
+			System.out.println("fuel: "+fuel+" left");
 			return;
 		}
-		try {
-		throw new NotEnoughFuelException();
-		}
-		catch(NotEnoughFuelException e) {
-			e.doExcept(this);
-		}
-		finally {
+
+		catch(CarException e){
+			System.out.println(e.getMessage());
+			e.doException(this);
 			this.run(dist);
+
 		}
+
+
+	
 	}
 
 }
--- java/exceptionTest/src/A6_exception5/NotEnoughFuelException.java
+++ java/exceptionTest/src/A6_exception5/NotEnoughFuelException.java
@@ -1,22 +1,16 @@
 package A6_exception5;
 
-public class NotEnoughFuelException extends Exception {
+public class NotEnoughFuelException extends CarException {
 	public NotEnoughFuelException() {
-		this("fuel is done");//default message
+		super("fuel is done");
 	}
-	public NotEnoughFuelException(String msg) {
-		super(msg);
-	}
-	public void doExcept(Car car) {
+
+	public void doException(Car car) {
 		System.out.println("recharge fuel to 100L");
 		car.setFuel(100);
-		System.out.println("end recharge now let's go");
-		car.run(100);
 	}
-	public void doExcept(Car car,int dist) {
+	public void doException(Car car,int dist) {
 		System.out.println("recharge fuel to 100L");
 		car.setFuel(100);
-		System.out.println("end recharge now let's go");
-		car.run(dist);
 	}
 }
