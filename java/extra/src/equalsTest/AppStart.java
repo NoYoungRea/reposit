3308a3bf359eec5b848885e7c994f3f7740d2498
NoYoungRea <rea2169@naver.com>
2020-03-17 15:14:20 +0900

    7

--- java/extra/src/equalsTest/AppStart.java
+++ java/extra/src/equalsTest/AppStart.java
@@ -2,15 +2,13 @@ package equalsTest;
 
 public class AppStart {
 	public static void main(String[] args) {
-		Manager p1=new Manager(new Student("hgd",20,"math",30));
-		Manager p2=new Manager(new Student("hgd",20,"math",30));
-		Manager p3=p1;;
-		
-		System.out.println(p1==p2);
-		System.out.println(p1==p3);
-		System.out.println(p1.equals(p2));
-		System.out.println(p1.equals(p3));
 
+		Student s=new Student("gd",20,"math",30);
+		Person p=new Student("gd",20,"math",30);
+		Object o=new Student("gd",20,"math",30);
+
+		System.out.println(s.equals(p));
+		System.out.println(o.equals(s));
 		
 	}
 }
