197ac4d299d8439886327f8843f46d468bbb0609
NoYoungRea <rea2169@naver.com>
2020-03-17 17:40:14 +0900

    3

--- java/extra/src/CloneTest/AppStart.java
+++ java/extra/src/CloneTest/AppStart.java
@@ -3,9 +3,9 @@ package CloneTest;
 public class AppStart {
 	public static void main(String[] args) {
 
-		SportCar c = new SportCar("benz", "Black",30);
+		Object c = new SportCar("benz", "Black",30);
 		try {
-			SportCar newCar = (SportCar)c.clone();
+			SportCar newCar = (SportCar)((SportCar)c).clone();
 			System.out.println(c.toString());
 			System.out.println(newCar.toString());
 		} catch (CloneNotSupportedException e) {
