6877b1c1a86132914e8f118b830b77c1d0d3d63f
NoYoungRea <rea2169@naver.com>
2020-03-18 01:06:52 +0900

    1

---
+++ designPattern/factory/src/PizzaHut.java
@@ -0,0 +1,18 @@
+public class PizzaHut extends PizzaStore {
+	public PizzaHut() {
+		super("pizzaHut");
+
+	}
+
+	public Pizza order(String name) {
+		Pizza pizza = null;
+		if (name.equals(BaconPizza.NAME)) {
+			pizza = new BaconPizza();
+		} else if (name.equals(CheesePizza.NAME)) {
+			pizza = new CheesePizza();
+		} else {
+			;
+		}
+		return pizza;
+	}
+}
---
+++ designPattern/factory/src/PizzaStore.java
@@ -0,0 +1,18 @@
+public abstract class PizzaStore {
+
+	protected String name;
+
+	public PizzaStore(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public abstract Pizza order(String name);
+}
