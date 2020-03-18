125e8d71df9977ba38d823bc6281867520e14819
NoYoungRea <rea2169@naver.com>
2020-03-18 10:04:55 +0900

    1

---
+++ java/java_a1/src/hashCodeTest/AppStart.java
@@ -0,0 +1,14 @@
+package hashCodeTest;
+
+public class AppStart {
+	public static void main(String[]args) {
+		Object o1=new Object();
+		Object o2=new Object();
+		System.out.println(o1);
+		System.out.println(Integer.toHexString(o1.hashCode()));
+		System.out.println(o2);
+		System.out.println(Integer.toHexString(o1.hashCode()));	
+		System.out.println(o1.getClass().getName());
+		}
+	
+}
