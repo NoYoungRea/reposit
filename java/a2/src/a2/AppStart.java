c19cab2b5ded24fb710e13f2ffd16d54cdb70918
NoYoungRea <rea2169@naver.com>
2020-03-18 11:26:14 +0900

    11

--- java/a2/src/a2/AppStart.java
+++ java/a2/src/a2/AppStart.java
@@ -2,15 +2,15 @@ package a2;
 
 import java.util.Comparator;
 import java.util.Objects;
-class MyComparator implements Comparator<Integer>{
-	public int compare(Integer a,Integer b	) {
-		return (a-b);
-	}
-}
+
 public class AppStart {
 	public static void main(String[]args) {
 		int a=10;
 		int b=5;
-		int c=Objects.compare(a, b, new MyComparator());
+		int c=Objects.compare(a, b, new Comparator<Integer>() {
+			public int compare(Integer a,Integer b) {
+				return (a-b);
+			}
+		});
 	}
 }
