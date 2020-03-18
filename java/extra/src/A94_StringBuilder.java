7f5a0360ddd47949160b4db2bc743fb3076537b5
NoYoungRea <rea2169@naver.com>
2020-03-18 15:46:18 +0900

    5

---
+++ java/extra/src/A94_StringBuilder.java
@@ -0,0 +1,9 @@
+
+public class A94_StringBuilder {
+	public static void main(String[]args) {
+		StringBuilder sb=new StringBuilder();
+		sb.append("one").append("two").append("Three");
+		System.out.println(sb.reverse());
+		
+	}
+}
