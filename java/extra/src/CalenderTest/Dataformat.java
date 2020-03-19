46ac6d3646dc272e7b83d1af115e7d0f69287061
NoYoungRea <rea2169@naver.com>
2020-03-19 15:09:01 +0900

    4

--- java/extra/src/CalenderTest/Dataformat.java
+++ java/extra/src/CalenderTest/Dataformat.java
@@ -4,10 +4,18 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class Dataformat {
-	public static void main(String[]args) {
-		Calendar cal=Calendar.getInstance();
-		SimpleDateFormat sdf=new SimpleDateFormat("yyyy MM dd HH mm ss");
-		String date=sdf.format(cal.getTime());
-		System.out.println(date);
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			Calendar cal = Calendar.getInstance();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy MM dd HH mm ss");
+			System.out.println(cal.getTime());
+			String date = sdf.format(cal.getTime());
+			System.out.println(date);
+
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+			}
+		}
 	}
 }
