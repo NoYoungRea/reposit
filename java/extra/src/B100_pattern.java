cb5332ef000a111a22c4e50da8e8bdda5801c9a2
NoYoungRea <rea2169@naver.com>
2020-03-19 10:39:46 +0900

    3

--- java/extra/src/B100_pattern.java
+++ java/extra/src/B100_pattern.java
@@ -1,13 +1,28 @@
 import java.util.regex.Pattern;
 
 class PatternChecker {
+	private static String phoneNumber ="\\d{2,3}-\\d{3,4}-\\d{4}";
+	private static String email="[a-z][\\w_]*@\\w+\\.(com|net|mil|gov)|((co|ac|ne)\\.(kr|jp))";
+	private static String id;
+	private static String password="[\\w!@#$%^&*]{6,15}";
+	
+	public static String getId() {
+		return id;
+	}
+	public static void setId(String id) {
+		PatternChecker.id = id;
+	}
 	public static boolean checkPhoneNumber(String myPhoneNumber) {
-		String phoneNumber ="\\d{2,3}-\\d{3,4}-\\d{4}";
 		return (Pattern.matches(phoneNumber, myPhoneNumber));
 	}
+	public static boolean checkId(String myId) {
+		return (Pattern.matches(id, myId));
+	}
 	public static boolean checkEmail(String myEmail) {
-		String email="[a-z][\\w_]*@\\w+\\.(com|net|mil|gov)|((co|ac|ne)\\.(kr|jp))";
 		return (Pattern.matches(email, myEmail));
+	}
+	public static boolean checkPassword(String myPassword) {
+		return (Pattern.matches(password, myPassword));
 	}
 }
 
@@ -15,5 +30,11 @@ public class B100_pattern {
 	public static void main(String[] args) {
 		System.out.println(PatternChecker.checkEmail("rea2169@naver.com"));
 		System.out.println(PatternChecker.checkPhoneNumber("010-3023-2169"));
+		System.out.println(PatternChecker.checkPassword("dudfo123Q!"));
+		PatternChecker.setId("[a-z]{6,10}");
+		System.out.println(PatternChecker.checkId("shdudfo"));
+
+		
+
 	}
 }
