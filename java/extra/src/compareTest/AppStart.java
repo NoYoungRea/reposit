86ec2baafb381eab694531a7a24579d4e099ce69
NoYoungRea <rea2169@naver.com>
2020-03-19 11:31:53 +0900

    6

--- java/extra/src/compareTest/AppStart.java
+++ java/extra/src/compareTest/AppStart.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class AppStart {
 	public static void main(String[] args) {
-		Member[] members = { new Member("abc"), new Member("cde"), new Member("abde") };
+		Member[] members = { new Member("abc",20,30), new Member("cde",30,20), new Member("abde",40,50) };
 		Arrays.sort(members);
 		for (Member m : members) {
-			System.out.println(m.getName());
+			System.out.println(m);
 		}
 	}
 }
--- java/extra/src/compareTest/Member.java
+++ java/extra/src/compareTest/Member.java
@@ -3,9 +3,29 @@ package compareTest;
 public class Member implements Comparable<Member>{
 
 	private String name;
+	private int kor;
+	private int eng;
 	
-	public Member(String name) {
+	public Member(String name,int kor,int eng) {
 		this.name=name;
+		this.kor=kor;
+		this.eng=eng;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
 	}
 
 	public String getName() {
@@ -16,6 +36,14 @@ public class Member implements Comparable<Member>{
 		this.name = name;
 	}
 	public int compareTo(Member o) {
-		return -this.name.compareTo(o.name);
+		Integer a=new Integer(sum(this));
+		Integer b=new Integer(sum(o));
+		return -a.compareTo(b);
+	}
+	public int sum(Member o) {
+		return o.eng+o.kor;
+	}
+	public String toString() {
+		return String.format("%8s %4d %4d %4d %7.2f", name,kor,eng,(kor+eng),(kor+eng)/2.0);
 	}
 }
