7c92a97abddc44ec6f67d9f85495f4f5d3bf309f
NoYoungRea <rea2169@naver.com>
2020-03-19 14:40:06 +0900

    2

--- java/extra/src/CalenderTest/AppStart.java
+++ java/extra/src/CalenderTest/AppStart.java
@@ -6,7 +6,6 @@ public class AppStart {
 	public static void main(String[] args) {
 		Calendar cal = Calendar.getInstance();
 		StringBuilder sb = new StringBuilder();
-
 		sb.append(cal.get(Calendar.YEAR)).append("year")
 		.append(cal.get(Calendar.MONTH)+1).append("month")
 		.append(cal.get(Calendar.DAY_OF_MONTH)).append("day")
@@ -14,5 +13,17 @@ public class AppStart {
 		.append(cal.get(Calendar.MINUTE)).append("minute")
 		.append(cal.get(Calendar.SECOND)).append("second");
 		System.out.println(sb);
+		
+		cal.set(Calendar.HOUR, cal.get(Calendar.HOUR)+500);
+		cal.set(Calendar.DAY_OF_MONTH, cal.get(Calendar.DAY_OF_MONTH)+10000);
+		sb = new StringBuilder();
+		sb.append(cal.get(Calendar.YEAR)).append("year")
+		.append(cal.get(Calendar.MONTH)+1).append("month")
+		.append(cal.get(Calendar.DAY_OF_MONTH)).append("day")
+		.append(cal.get(Calendar.HOUR)).append("hour")
+		.append(cal.get(Calendar.MINUTE)).append("minute")
+		.append(cal.get(Calendar.SECOND)).append("second");
+		System.out.println(sb);
+		
 	}
 }
