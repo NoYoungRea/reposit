baf10a0e9475b7bffa78acdac27003103d6817f4
NoYoungRea <rea2169@naver.com>
2020-03-19 14:34:31 +0900

    1

---
+++ java/extra/src/CalenderTest/AppStart.java
@@ -0,0 +1,18 @@
+package CalenderTest;
+
+import java.util.Calendar;
+
+public class AppStart {
+	public static void main(String[] args) {
+		Calendar cal = Calendar.getInstance();
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(cal.get(Calendar.YEAR)).append("year")
+		.append(cal.get(Calendar.MONTH)+1).append("month")
+		.append(cal.get(Calendar.DAY_OF_MONTH)).append("day")
+		.append(cal.get(Calendar.HOUR)).append("hour")
+		.append(cal.get(Calendar.MINUTE)).append("minute")
+		.append(cal.get(Calendar.SECOND)).append("second");
+		System.out.println(sb);
+	}
+}
