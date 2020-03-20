466c00cc3ec008f40e871bffb80245392d14ea97
NoYoungRea <rea2169@naver.com>
2020-03-20 16:26:48 +0900

    15

--- java/generic/src/genericfinal/AppStart.java
+++ java/generic/src/genericfinal/AppStart.java
@@ -1,5 +1,38 @@
 package genericfinal;
 
-public class AppStart {
+class Person<K,V>{
+	private K name;
+	private V jumsu;
+	
+	public Person(K name, V jumsu) {
+		this.name=name;
+		this.jumsu=jumsu;
+	}
 
+	public K getName() {
+		return name;
+	}
+
+	public void setName(K name) {
+		this.name = name;
+	}
+
+	public V getJumsu() {
+		return jumsu;
+	}
+
+	public void setJumsu(V jumsu) {
+		this.jumsu = jumsu;
+	}
+	
+}
+
+public class AppStart {
+	public static<K,V> Person<K,V> func(K name,V jumsu) {
+		Person<K,V> person = new Person<>(name,jumsu);
+		return person;
+	}
+	public static void main(String[]args) {
+		Person<String, Integer> person=func("hgd",100);
+	}
 }
