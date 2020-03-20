cb7b2ad3656aa9442009d4b996240bc9970240b6
NoYoungRea <rea2169@naver.com>
2020-03-20 14:53:46 +0900

    2

--- java/generic/src/genericTest1/AppStart.java
+++ java/generic/src/genericTest1/AppStart.java
@@ -1,5 +1,17 @@
 package genericTest1;
 
+class A{
+	
+}
+class B extends A{
+	
+}
 public class AppStart {
-
+	public static void func(A obj) {
+		
+	}
+	public static void main(String[]args) {
+		B b=new B();
+		func(b);
+	}
 }
