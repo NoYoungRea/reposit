e7fb0863bd94410481c75ec2b8c724468b64337d
NoYoungRea <rea2169@naver.com>
2020-03-21 15:31:41 +0900

    1

---
+++ java/generic/src/genericb1/AppStart.java
@@ -0,0 +1,22 @@
+package genericb1;
+
+interface Flyable<T>{
+	
+}
+
+class Person<T>{
+	
+}
+
+class Duck implements Flyable<Integer>{
+	
+}
+class Duck1<T> implements Flyable<T>{
+	
+}
+
+public class AppStart {
+	public static void main(String[]args) {
+		
+	}
+}
