624f40e7b5b8eaab3fb4bb257faab8e02bc39e4d
NoYoungRea <rea2169@naver.com>
2020-03-21 16:09:28 +0900

    12

--- java/generic/src/genericb1/Course.java
+++ java/generic/src/genericb1/Course.java
@@ -1,15 +1,53 @@
 package genericb1;
 
 public class Course <T extends Person>{
-	private T student;
+	private String courseName;
+	private T[]students;
+	private int count;
 	
-	public Course(T student) {
-		this.student=student;
+	public Course(String courseName, int capacity) {
+		students=(T[])(new Object[capacity]);
+		this.courseName=courseName;
+		this.count=0;
 	}
-	public T getStudent() {
-		return student;
+
+	public String getCourseName() {
+		return courseName;
 	}
-	public void show() {
-		System.out.println(student.getName());
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
 	}
+
+	public T[] getStudents() {
+		return students;
+	}
+
+	public void setStudents(T[] students) {
+		this.students = students;
+	}
+	
+	public void add(T student) {
+		if (count<students.length) {
+			students[count++]=student;
+		}
+	}
+	public void showLis() {
+		for(T obj:students) {
+			if(obj instanceof CollageStudent) {
+	
+			}
+			else if(obj instanceof Student) {
+				
+			}
+			else if(obj instanceof Worker) {
+				
+			}
+			else if(obj instanceof Person) {	
+			
+		}
+			
+		}
+	}
+
 }
