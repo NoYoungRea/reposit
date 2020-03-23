05994d470bc1cf25acbf258fb90d69c7b4567bc7
NoYoungRea <rea2169@naver.com>
2020-03-23 11:15:14 +0900

    4

--- java/Thread/src/ThreadTest2/ThreadTest2.java
+++ java/Thread/src/ThreadTest2/ThreadTest2.java
@@ -1,9 +1,35 @@
 package ThreadTest2;
-
+class MyThread extends Thread{
+	protected int a;
+	public MyThread(int a) {
+		this.a=a;
+	}
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+}
 public class ThreadTest2 {
 	public static void main(String[] args) {
 		int a=10;
 
+		Thread t0= new MyThread(100) {
+			public void run() {
+				for(int i=0;i<10;i++) {
+					a++;
+					try {
+						Thread.sleep(500);
+					}
+					catch(InterruptedException e) {
+						
+					}
+					System.out.println(a);
+				}
+			}
+		};
+		
 		Thread t1 = new Thread("first") {
 			public void run() {
 				for (int i = 0; i < 10; i++) {
@@ -35,7 +61,7 @@ public class ThreadTest2 {
 			}
 		},"second thread");
 
-		
+		t0.start();
 		t1.start();
 		t2.start();
 		System.out.println(Thread.currentThread().getName() + "/process exit");
