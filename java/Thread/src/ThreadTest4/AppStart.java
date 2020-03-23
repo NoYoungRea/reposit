a0bd8fd8b64106ea0dacd673f78466eaec1fe2cd
NoYoungRea <rea2169@naver.com>
2020-03-23 11:53:30 +0900

    1

---
+++ java/Thread/src/ThreadTest4/AppStart.java
@@ -0,0 +1,37 @@
+package ThreadTest4;
+
+class Computer extends Thread {
+	private int result;
+
+	public int getResult() {
+		return result;
+	}
+
+	public void setResult(int result) {
+		this.result = result;
+	}
+
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			result += i;
+			try {
+				Thread.sleep(100);
+
+			} catch (InterruptedException e) {
+
+			}
+			System.out.println("succdd");
+
+		}
+	}
+}
+
+public class AppStart {
+	public static void main(String []args) {
+		Thread t=new Computer();
+		t.start();
+		System.out.println("result/"+((Computer)t).getResult());
+	}
+}
+
+
