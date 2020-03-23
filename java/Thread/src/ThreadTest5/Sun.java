267993e360a074c7584a058704fe0f8da8f425f0
NoYoungRea <rea2169@naver.com>
2020-03-23 12:42:02 +0900

    4

--- java/Thread/src/ThreadTest5/Sun.java
+++ java/Thread/src/ThreadTest5/Sun.java
@@ -7,9 +7,13 @@ public class Sun extends Thread{
 		this.bank=bank;
 	}
 	public void run() {
+		int i=0;
 		while(!bank.GetisReady()) {
+			i++;
 			System.out.println("mom is not already");
+			
 		}
+		System.out.println(i);
 		int money=bank.withdraw(1000);
 		System.out.println("sun/"+money+"/withdraw");
 
