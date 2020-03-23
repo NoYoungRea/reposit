4142d7946af32925a6b61505f6452252dfc4e0af
NoYoungRea <rea2169@naver.com>
2020-03-23 14:28:24 +0900

    2

--- java/Thread/src/ThreadTest6/User.java
+++ java/Thread/src/ThreadTest6/User.java
@@ -1,31 +1,35 @@
 package ThreadTest6;
 
-public class User implements Runnable{
+public class User implements Runnable {
 	private String name;
 	private Bank bank;
-	
+
 	public User(String name, Bank bank) {
-		this.name=name;
-		this.bank=bank;
+		this.name = name;
+		this.bank = bank;
 	}
+
 	public void setBank(Bank bank) {
-		this.bank=bank;
+		this.bank = bank;
 	}
+
 	public Bank getBank() {
 		return bank;
 	}
+
 	public void run() {
-		for(int i=0;i<10;i++) {
+		for (int i = 0; i < 10; i++) {
 			try {
 				Thread.sleep(10);
+			} catch (InterruptedException e) {
+
 			}
-			catch(InterruptedException e) {
-				
+			int money = 1000;
+			synchronized (bank) {
+				money = bank.withdraw(1000);
+				System.out.println(name + " is " + money + "/withdraw");
+				System.out.println("balance is " + bank.getBalance());
 			}
-			int money=1000;
-			money=bank.withdraw(1000);
-			System.out.println(name+" is "+money+"/withdraw");
-			System.out.println("balance is "+bank.getBalance());
 		}
 	}
 }
