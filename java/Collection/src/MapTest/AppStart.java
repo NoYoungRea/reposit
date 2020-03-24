de3f8ef8b75608bab382ad03a161e8928602b100
NoYoungRea <rea2169@naver.com>
2020-03-24 11:18:19 +0900

    2

--- java/Collection/src/MapTest/AppStart.java
+++ java/Collection/src/MapTest/AppStart.java
@@ -1,6 +1,9 @@
 package MapTest;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class AppStart {
 	public static void main(String[]args) {
@@ -8,6 +11,17 @@ public class AppStart {
 		hm.put(1, "hgd");
 		hm.put(2, "asd");
 		hm.put(3, "dfk");
+		hm.put(3,"aaa");
+		
+		Set<Integer>keys=hm.keySet();
+		Iterator<Integer>keyIterator=keys.iterator();
+		while(keyIterator.hasNext()) {
+			System.out.println(keyIterator.next());
+			System.out.println(hm.get(keyIterator.next()));
+		}
+		
+		Set<Map.Entry<Integer,String>> entrySet=hm.entrySet();
+		
 		
 		if(hm.containsKey(1)) {
 			System.out.println(hm.get(1));
