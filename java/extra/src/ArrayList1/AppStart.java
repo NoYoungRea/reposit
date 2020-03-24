09d59aad55f088500763f5fd4d145332df6a9620
NoYoungRea <rea2169@naver.com>
2020-03-24 09:58:28 +0900

    1

---
+++ java/extra/src/ArrayList1/AppStart.java
@@ -0,0 +1,19 @@
+package ArrayList1;
+
+import java.util.ArrayList;
+
+public class AppStart {
+	public static void main(String[]args) {
+		ArrayList al=new ArrayList(3);
+		al.add(10);
+		al.add(0,20);
+		al.add(1,30);
+		al.add(0,50);
+		
+		for(int i=0;i<al.size();i++) {
+			Object o=al.get(i);
+			System.out.println(o);
+
+		}
+	}
+}
