5cc0510659323d7a2edce203e7cdfe66e543fbef
NoYoungRea <rea2169@naver.com>
2020-03-24 14:03:55 +0900

    2

--- java/Collection/src/HashSet/AppStart.java
+++ java/Collection/src/HashSet/AppStart.java
@@ -4,18 +4,30 @@ import java.util.HashSet;
 import java.util.Iterator;
 
 public class AppStart {
-	public static void main(String[]args) {
-		HashSet<Person> hs=new HashSet<>();
+	public static void main(String[] args) {
+		HashSet<Person> hs = new HashSet<>();
 		hs.add(new Person("aa"));
 		hs.add(new Person("bb"));
 		hs.add(new Person("aa"));
-		
-		
-		Iterator<Person> it=hs.iterator();
-		String name="aa";
 
-		
-		while(it.hasNext()) {
+		Iterator<Person> it = hs.iterator();
+		String name = "aa";
+
+		Person d = null;
+
+		while (it.hasNext()) {
+			Person p = it.next();
+
+			if (p.getName().equals(name)) {
+				d = p;
+				break;
+			}
+		}
+		hs.remove(d);
+
+		it = hs.iterator();
+
+		while (it.hasNext()) {
 			System.out.println(it.next());
 		}
 	}
