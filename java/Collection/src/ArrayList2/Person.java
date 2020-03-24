016f71481387f07970fd352b0837cbc4d568fa30
NoYoungRea <rea2169@naver.com>
2020-03-24 10:18:34 +0900

    14

--- java/Collection/src/ArrayList2/Person.java
+++ java/Collection/src/ArrayList2/Person.java
@@ -18,4 +18,20 @@ public class Person {
 	public String toString() {
 		return this.getClass().getName()+"/"+name;
 	}
+	public boolean equals(Object obj) {
+		if(obj==null) {
+			return false;
+		}
+		if(this.getClass().getName()!=obj.getClass().getName())
+			return false;
+		Person o=((Person)obj);	
+		
+		if(name!=null) {
+			if(!name.equals(o.name))return false;
+		}
+		else
+			if(o.name!=null)return false;
+		return true;
+		
+	}
 }
