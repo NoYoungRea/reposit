8d82f9ee7c16f494d27f23b4e0309a568c813865
NoYoungRea <rea2169@naver.com>
2020-03-25 11:55:44 +0900

    11

--- java/File/src/outputStream/AppStart.java
+++ java/File/src/outputStream/AppStart.java
@@ -1,17 +1,23 @@
 package outputStream;
 
+import java.io.DataOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class AppStart {
 	public static void main(String[]args) {
+		DataOutputStream dos=null;
 		FileOutputStream fos=null;
 		try {
 			fos=new FileOutputStream("c:/tmp/data.txt");
+			dos=new DataOutputStream(fos);
 			fos.write(65);
 			fos.write(66);
 			fos.write(166);
+			dos.writeDouble(3.14);
+			dos.writeChar('A');
+			dos.writeChars("shdudfo");
 
 		}
 		catch(FileNotFoundException e) {
@@ -22,7 +28,7 @@ public class AppStart {
 		}
 		finally {
 			try {
-				fos.close();
+				dos.close();//dos closed with fos
 			}
 			catch(IOException |NullPointerException e) {
 				e.printStackTrace();
