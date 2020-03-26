03f36d6f4891cc2c4035bc1164046652f189c056
NoYoungRea <rea2169@naver.com>
2020-03-26 11:31:31 +0900

    6

--- java/File/src/ReaderWriter2/Leader.java
+++ java/File/src/ReaderWriter2/Leader.java
@@ -2,17 +2,34 @@ package ReaderWriter2;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.StringTokenizer;
 
 public class Leader {
 	public static void main(String[]args) {
 		BufferedReader in=null;
+		StringTokenizer a=null;
 		try {
 			in=new BufferedReader(new FileReader("C:/tmp/data.txt"),1024);
 			while(true) {
 				String line=in.readLine();
-				System.out.println(line);
+				if(line==null){
+					break;
+				}
+				a=new StringTokenizer(line,":");
+				if(a.countTokens()!=2) {
+					System.out.println("there are some problem");
+				}
+				else {
+					String name=a.nextToken();
+					int age=Integer.parseInt(a.nextToken());	
+					System.out.println("name/"+name+" : "+"age/"+age);
+				}
+
 			}
-		}catch(Exception e) {
+		}catch(NumberFormatException e) {
+			System.out.println("there are some probelm from parsein");
+		}
+		catch(Exception e) {
 			e.printStackTrace();
 		}finally {
 			try{in.close();}catch(Exception e) {}
