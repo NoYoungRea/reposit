a588383a4788ffe5d9ce2bd11c3d28c687b97e4b
NoYoungRea <rea2169@naver.com>
2020-03-26 10:45:52 +0900

    2

--- java/File/src/ReaderWriter2/AppStart.java
+++ java/File/src/ReaderWriter2/AppStart.java
@@ -1,16 +1,25 @@
 package ReaderWriter2;
 
 import java.io.FileWriter;
+import java.io.PrintWriter;
 
 public class AppStart {
 	public static void main(String[] args) {
 		FileWriter fw = null;
+		PrintWriter out=null;
 		try{
-			fw=new FileWriter("C:/temp/data.txt");
-			String str="abcedfg";
-			int pos=2;
-			fw.write(str,1,str.length()-pos);
+			fw=new FileWriter("C:/tmp/data.txt");
+			out=new PrintWriter(fw);
+			out.println(100);
+			out.println(3.14);
+			out.println("hello");
+			out.printf("%d:%.2f:%s\n",100,3.14,"hello");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			out.close();
 		}
+		
 	}
 }
 
