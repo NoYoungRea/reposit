f01c9c79e03d002648d82c78d354f6237dbc84fc
NoYoungRea <rea2169@naver.com>
2020-03-28 15:49:36 +0900

    3

--- java/ramda/src/Test1/AppStart.java
+++ java/ramda/src/Test1/AppStart.java
@@ -1,12 +1,18 @@
 package Test1;
 
+interface Printable{
+	void print(String name);
+}
 public class AppStart {
 
 	public static void main(String[] args) {
 	
 		Runnable runnable =()->{
+			//(¤·)
 		};
-		
+		Printable printable=(name)->{
+			
+		};
 		
 	}
 	
