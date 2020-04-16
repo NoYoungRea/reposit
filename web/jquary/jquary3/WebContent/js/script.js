d140e0ab13a77d4a6090e021b878a5d93237ae6c
NoYoungRea <rea2169@naver.com>
2020-04-16 12:52:43 +0900

    1

--- web/jquary/jquary3/WebContent/js/script.js
+++ web/jquary/jquary3/WebContent/js/script.js
@@ -1,24 +1,19 @@
-var popupHtml = ''
-+	'<div class="popup">'
-+ '<ul>'
-+	'<li class="message">'
-+	'</li>'
-+	'<li class="action">'
-+		'<button class="btn btn-blue" type="button">닫기</button>'
-+	'</li>'
-+ '</ul>'
-+ '</div>';
-
-
-$(document).ready(function(){
-	$('.openPopup').click(function(){
-		let popup = $(popupHtml);
-		popup.find('.message').text('팝업창 메세지를 입력합니다.');
-		popup.find('button').click(function(){
-			$(this).closest('.popup').remove();  
-			//this는 윗줄 button //closest는 버튼을 감싸고있는것들중 popup인것
+$.fn.openPopup=function(msg,func){
+	this.click(function(){
+		$('.popup').find('.message').text(msg);
+		$('.popup').find('button').click(function(){
+			if(func){
+				func();
+			}
+			$(this).closest('.popup').css('display','none');
+			$(this).off('click')
 		});
-		popup.appendTo($('body'));
+		$('.popup').css('display','block');
+	})
+}
+$(document).ready(function(){
+	$('.openPopup').openPopup("데이터를 전송하시겠습니까?",function(){
+		alert('팝업을 닫기전에 작업을 수행합니다.')
 	});
 });
 
