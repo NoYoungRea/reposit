47cc8527c869d4141ed477df5175db8fe904bc6e
NoYoungRea <rea2169@naver.com>
2020-04-17 15:41:52 +0900

    server1

--- web/NodeJsByEclipse/node2/WebContent/server.js
+++ web/NodeJsByEclipse/node2/WebContent/server.js
@@ -1,19 +1,15 @@
-const port = 80;
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
-var fs = require('fs');
-app.listen(port, function(){
-	console.log(port+'포트로 서버구동하였습니다.');
-});
+const port=80;
+var express= require('express');
+var app=express();
+var io=require('socket.io')(8080)
 
-function handler(request, response) {
-	fs.readFile(__dirname+'/index.html', function(error, data){
-		if(error){
-			response.writeHead(500);
-			return response.end('지정한 파일을 찾을수없습니다.');
-			
-		}
-		response.writeHead(200);
-		return response.end(data);
-	});
-}
+app.listen(port,function(){
+	console.log('by'+port)
+})
+
+app.use(express.static('.'));
+
+io.on('connection',function(socket){
+	let id=socket.id
+	console.log('client :'+id)
+})//like java's accpte
