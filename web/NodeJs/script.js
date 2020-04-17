4e1275c61d4a44b508f27788b37f22811afdcb0b
NoYoungRea <rea2169@naver.com>
2020-04-17 12:21:59 +0900

    get

--- web/NodeJs/script.js
+++ web/NodeJs/script.js
@@ -5,12 +5,17 @@ var server=exp();
 server.listen(port,function(){
 	console.log(port+'from port we start server')
 })
-//서버를 열고 외부에서 연결을 기다리는 상태(80번 포트에 소켓을 만들어 바인딩을 해 놓은 상태)
 
 server.use(exp.static('public'));
-//여기를 먼저 뒤지라는 것이다. 응답을 못하면 다음으로 넘어감
-server.use(function(req,res){
-	res.send('안녕!')
+//정적 파일 route
+
+http://localhost or http://localhost/주소 get 방식 요청->이게 default이다.
+server.get('/',function(request,response){
+	console.log('요청주소 : http://localhost, method: get');
+	response.send('get방식 요청에 대한 응답');
+})
+server.post('/',function(request,response){
+	console.log('요청주소 : http://localhost, method: post');
+	response.send('post방식 요청에 대한 응답');
 })
 
-// localhost 입력해보고 localhost/1.jpg 입력해보기 무언가 입력했을때 무조건 public 안 부터 뒤짐
