8d107f8f5fbfb30dd17f29e52a745ccef89b789a
NoYoungRea <rea2169@naver.com>
2020-04-17 12:52:47 +0900

    url

--- web/NodeJs/script.js
+++ web/NodeJs/script.js
@@ -4,30 +4,16 @@ var server=exp();
 
 server.listen(port,function(){
 	console.log(port+'from port we start server')
+
 })
 
 server.use(exp.static('public'));
 //정적 파일 route
 
 
+//http://localhost/board?name="hgd"
 server.get('/board',function(request,response){
-	console.log('게시글 목록보기 요청이 접수되었습니다.');
-	response.send('게시글 목록보기 요청이 접수됨');
+	console.log(request.query.name);
 })
-server.post('/board',function(request,response){
-	console.log('게시글 등록 요청이 접수되었습니다.');
-	response.send('게시글 등록 요청이 접수됨');
-})
-server.delete('/board',function(request,response){
-	console.log('게시글 삭제 요청이 접수되었습니다.');
-	response.send('게시글 삭제 요청이 접수됨');
-})
-server.patch('/board',function(request,response){
-	console.log('게시글 편집 요청이 접수되었습니다.');
-	response.send('게시글 편집 요청이 접수됨');
-})
-server.all('/board',function(request,response){
-	console.log('전부');
-	response.send('전부');
-})
+
 
