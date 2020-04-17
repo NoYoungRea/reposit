04bf4d2de541631b044fad06d61280c7a22d8bb0
NoYoungRea <rea2169@naver.com>
2020-04-17 11:37:32 +0900

    express

--- web/NodeJs/script.js
+++ web/NodeJs/script.js
@@ -1,9 +1,8 @@
-var req=require('request');
-req('http://www.naver.com',function(err,res,body){
-	if(err){
-		console.log('error is occured')
-	}
-	else{
-		console.log(body)
-	}
-});
+const port=80;
+var exp=require('express')
+var server=exp();
+
+server.listen(port,function(){
+	console.log(port+'from port we start server')
+})
+//서버를 열고 외부에서 연결을 기다리는 상태(80번 포트에 소켓을 만들어 바인딩을 해 놓은 상태)
