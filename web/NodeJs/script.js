4f69b93d3020fcf01a2666299024c64f3f0496d2
NoYoungRea <rea2169@naver.com>
2020-04-17 11:20:01 +0900

    sort

--- web/NodeJs/script.js
+++ web/NodeJs/script.js
@@ -1,8 +1,21 @@
-var jumsu=[90,30,40,10,50,70,30,40];
+var students=[
+	{name:'hgd',kor:90,eng:83,math:93},
+	{name:'hgd',kor:93,eng:87,math:93},
+	{name:'hgd',kor:96,eng:89,math:93},
+	{name:'hgd',kor:95,eng:82,math:93},
+	{name:'hgd',kor:92,eng:83,math:93},
+	{name:'hgd',kor:98,eng:85,math:93},
+	{name:'hgd',kor:100,eng:87,math:93},
+	{name:'hgd',kor:93,eng:83,math:93},
+	{name:'hgd',kor:99,eng:82,math:93},
+	{name:'hgd',kor:92,eng:81,math:93},
+	
+]
+students.sort(function(a,b){
+	
+	return a.kor+a.eng+a.math-b.kor+b.eng+b.math
+})
 
-jumsu.sort(function(a,b){
-	return (a-b);
-});
-jumsu.forEach(function(item,index){
-	console.log(jumsu[index]);
+students.forEach(function(item,index){
+	console.log(students[index])
 })
