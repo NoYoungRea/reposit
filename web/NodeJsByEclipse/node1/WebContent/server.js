15056c808b939766b8a6ed6707b2d4db26e6ee7c
NoYoungRea <rea2169@naver.com>
2020-04-17 14:44:19 +0900

    justResponseNotPush

--- web/NodeJsByEclipse/node1/WebContent/server.js
+++ web/NodeJsByEclipse/node1/WebContent/server.js
@@ -1,15 +1,29 @@
 const port=80;
 var express=require('express');
 var app=express();
+var parser=require('body-parser');
 
 app.listen(port,function(){
 	console.log('from port'+port);
 })
+
+app.use(parser.urlencoded({extended:false}));
 app.use(express.static('.'))
 //1.jpg 실험
 
 //http://localhost/board/1000
-app.get('/board/:number',function(request,response){
-	console.log(request.params.number);
-	response.send(request.params.number);
+app.post('/board',function(request,response){
+	let name=request.body.name;
+	let age=request.body.age;
+	console.log(name+':'+age);
+	if(name!='hgd'){
+		response.send({result:false,message:'이름이 일치하지 않습니다.'});
+	}
+	else if(age!=20){
+		response.send({result:false,message:'연령이 일치하지 않습니다.'});	
+	}
+	else{
+		response.send({result:false,message:'정상적으로 로그인'});	
+			
+	}
 })
