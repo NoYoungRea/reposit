441bda0078c8a47080c97d1f8298ad36d98283c3
NoYoungRea <rea2169@naver.com>
2020-04-17 11:31:18 +0900

    request

--- web/NodeJs/script.js
+++ web/NodeJs/script.js
@@ -1,3 +1,9 @@
-var os=require('os');
-console.log(os.hostname())
-//require를 이용하여 이용하고 싶을 모듈을 추출해야 한다.
+var req=require('request');
+req('http://www.naver.com',function(err,res,body){
+	if(err){
+		console.log('error is occured')
+	}
+	else{
+		console.log(body)
+	}
+});
