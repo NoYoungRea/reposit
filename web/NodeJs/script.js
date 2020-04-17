65605cac97fc218f562dbfe628c5187259a7891e
NoYoungRea <rea2169@naver.com>
2020-04-17 12:59:27 +0900

    QuerybyGet

--- web/NodeJs/script.js
+++ web/NodeJs/script.js
@@ -11,9 +11,16 @@ server.use(exp.static('public'));
 //정적 파일 route
 
 
-//http://localhost/board?name="hgd"
+//http://localhost/board?name=%22hgd%22&age=%2220
+//이건 get 방식으로 하는 것!!!
 server.get('/board',function(request,response){
-	console.log(request.query.name);
+	let jobj=Array();
+	for(let key in request.query){
+		let obj=new Object;
+		obj[key]=request.query[key];
+		jobj.push(obj);
+	}
+	response.send(jobj);
 })
 
 
