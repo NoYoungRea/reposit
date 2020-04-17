453fe3fa6f43f6691508e0a1195b7ab5743e0650
NoYoungRea <rea2169@naver.com>
2020-04-17 11:23:27 +0900

    require

--- web/NodeJs/script.js
+++ web/NodeJs/script.js
@@ -1,21 +1,3 @@
-var students=[
-	{name:'hgd',kor:90,eng:83,math:93},
-	{name:'hgd',kor:93,eng:87,math:93},
-	{name:'hgd',kor:96,eng:89,math:93},
-	{name:'hgd',kor:95,eng:82,math:93},
-	{name:'hgd',kor:92,eng:83,math:93},
-	{name:'hgd',kor:98,eng:85,math:93},
-	{name:'hgd',kor:100,eng:87,math:93},
-	{name:'hgd',kor:93,eng:83,math:93},
-	{name:'hgd',kor:99,eng:82,math:93},
-	{name:'hgd',kor:92,eng:81,math:93},
-	
-]
-students.sort(function(a,b){
-	
-	return a.kor+a.eng+a.math-b.kor+b.eng+b.math
-})
-
-students.forEach(function(item,index){
-	console.log(students[index])
-})
+var os=require('os');
+console.log(os.hostname())
+//require를 이용하여 이용하고 싶을 모듈을 추출해야 한다.
