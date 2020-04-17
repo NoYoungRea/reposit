53226c4577db8684b9098a1fbb1eb5c305cdd956
NoYoungRea <rea2169@naver.com>
2020-04-17 19:07:07 +0900

    server3

--- web/NodeJsByEclipse/node2/WebContent/server.js
+++ web/NodeJsByEclipse/node2/WebContent/server.js
@@ -8,12 +8,20 @@ app.listen(port,function(){
 })
 
 app.use(express.static('.'));
-
+	
 io.on('connection',function(socket){
 	let id=socket.id
+	let user=null;
 	console.log('client :'+id)
 	
-	socket.on('message',function(data){
-		io.emit('message','from server : '+data);
+
+	socket.emit('hello','채팅서버에 접속되었습니다.');
+	
+	socket.on('who',function(data){
+		user=data;
+		console.log('소켓 ['+id+']의 사용자명이['+user+']으로 결정되었습니다.');
+	})
+	socket.on('msg',function(data){
+		io.emit('msg',user+'>'+data);
 	})
 })//like java's accpte
