bc03e5932ee783a18eab8e873523b9b0b6b010e7
NoYoungRea <rea2169@naver.com>
2020-04-18 15:30:52 +0900

    server4

--- web/NodeJsByEclipse/node3/WebContent/server2.js
+++ web/NodeJsByEclipse/node3/WebContent/server2.js
@@ -1,22 +1,27 @@
-const port=80;
-var express =require("express");
-var mysql=require('mysql');
-var bodyParser=require('body-parser');
+const PORT = 80;
+var express = require('express');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
 
-var app=express();
-var conn=mysql.createConnection({
-	host:'localhost',
-	user:'nodejs',
-	password:'1234',
-	database:'nodejs'
-})
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(express.static('.'));
-app.listen(port,function(){
-	console.log('from'+port)
-})
-app.get('/members',function(request,response){
-	conn.query('select*from members',function(error,result,fields){
-		response,send(result);
-	})
-})
+var app = express();
+var conn = mysql.createConnection({
+   host: 'localhost',
+   user: 'nodejs',
+   password: '1234',
+   database: 'nodejs'
+});
+
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(express.static('.')); //현재파일로부터 요청파일을 찾게끔
+
+app.listen(PORT, function(){
+   console.log(PORT+'포트로 서버를 구동하였습니다.');
+});
+
+app.get('/members', function(request, response){ //라우터 사용 //url은 members 사용(이 주소로 호출)
+   conn.query('SELECT * FROM members', function(error, result, fields){
+      //field는 꼭 받을 필요는없긴함. forEach로도 가능함. 쓸일없으면 안써도가능
+      response.send(result);
+      
+   });
+});
