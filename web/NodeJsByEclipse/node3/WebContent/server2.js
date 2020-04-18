4759b8cb553ef913b9eb3878d2fc22b68517517e
NoYoungRea <rea2169@naver.com>
2020-04-18 14:57:08 +0900

    server2

--- web/NodeJsByEclipse/node3/WebContent/server2.js
+++ web/NodeJsByEclipse/node3/WebContent/server2.js
@@ -4,8 +4,19 @@ var mysql=require('mysql');
 var bodyParser=require('body-parser');
 
 var app=express();
+var conn=mysql.createConnection({
+	host:'localhost',
+	user:'nodejs',
+	password:'1234',
+	database:'nodejs'
+})
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static('.'));
 app.listen(port,function(){
 	console.log('from'+port)
+})
+app.get('/members',function(request,response){
+	conn.query('select*from members',function(error,result,fields){
+		response,send(result);
+	})
 })
