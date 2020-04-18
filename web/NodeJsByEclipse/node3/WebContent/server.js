220de215cb92dfb0a0f536800bf7452d38494c2c
NoYoungRea <rea2169@naver.com>
2020-04-18 14:39:38 +0900

    conn.query2

--- web/NodeJsByEclipse/node3/WebContent/server.js
+++ web/NodeJsByEclipse/node3/WebContent/server.js
@@ -11,8 +11,19 @@ conn.query('SELECT*FROM members',function(error,result,fields){
 	if(error){
 		console.log('error')
 	}else{
-		console.log(result);
-		console.log(fields);
+	
+		result.forEach(function(row,index){
+			console.log('메일	:'+row[fields[0].name]);
+			console.log('닉네임:'+row[fields[1].name])
+			console.log('가입일자:'+row[fields[2].name])
+			if(row[fields[3].name!=null])
+				console.log('탈퇴일자:'+row[fields[3].name])
+			else
+				console.log('탈퇴일자: 가입중')
+					
+			console.log('---------------------------------')
+			
+		})
 	}
 	
 })
