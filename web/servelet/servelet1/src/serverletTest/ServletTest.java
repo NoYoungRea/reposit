f9c9fca452a1c27eb7bf92cff9bcb8087c2717f8
NoYoungRea <rea2169@naver.com>
2020-04-20 10:02:30 +0900

    serverlet

---
+++ web/servelet/servelet1/src/serverletTest/ServletTest.java
@@ -0,0 +1,27 @@
+package serverletTest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/ServletTest")
+public class ServletTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public ServletTest() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF8");
+		PrintWriter out =response.getWriter();
+		out.println("get method요청에 대한 응답");
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out =response.getWriter();
+		out.println("post method요청에 대한 응답");	
+	}
+
+}
