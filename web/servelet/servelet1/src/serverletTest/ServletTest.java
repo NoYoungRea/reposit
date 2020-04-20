e7de5463096cb7797936964ba19fe44182598427
NoYoungRea <rea2169@naver.com>
2020-04-20 11:02:54 +0900

    init

--- web/servelet/servelet1/src/serverletTest/ServletTest.java
+++ web/servelet/servelet1/src/serverletTest/ServletTest.java
@@ -3,25 +3,24 @@ package serverletTest;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-@WebServlet("/Servlet")
+@WebServlet("/second")
 public class ServletTest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     public ServletTest() {
         super();
     }
+    public void init(ServletConfig config)throws ServletException{
+    	System.out.println("initmethod is called");
+    	
+    }
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html; charset=UTF8");
-		PrintWriter out =response.getWriter();
-		out.println("get method요청에 대한 응답");
-	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out =response.getWriter();
-		out.println("post method요청에 대한 응답");	
+	   	System.out.println("doget is called");
 	}
 
 }
