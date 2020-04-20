fdcd2225dfabaf6c5b975f99ea7e8987bc73cee4
NoYoungRea <rea2169@naver.com>
2020-04-20 14:46:11 +0900

    dispatch

---
+++ web/servelet/servelet2/src/a1/a1.java
@@ -0,0 +1,26 @@
+package a1;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class a1
+ */
+@WebServlet("/dispatcher")
+public class a1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	RequestDispatcher dispatcher=request.getRequestDispatcher("/deligator");
+	dispatcher.forward(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
