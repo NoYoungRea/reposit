c3efd361dd7b89742c1f2866e2c46d71f13d1234
NoYoungRea <rea2169@naver.com>
2020-04-23 15:47:29 +0900

    controller3

--- web/servelet/controllerProject/src/kr/co/shdudfo/controller/FrontController.java
+++ web/servelet/controllerProject/src/kr/co/shdudfo/controller/FrontController.java
@@ -1,31 +1,34 @@
 package kr.co.shdudfo.controller;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.HashMap;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import kr.co.shdudfo.service.BoardService;
-import kr.co.shdudfo.service.MemberService;
 import kr.co.shdudfo.service.ServiceController;
-import kr.co.shdudfo.service.UnkownService;
 
-@WebServlet("/*")
+
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	HashMap<String,ServiceController>services;
 	
 	public void init(ServletConfig config)throws ServletException {
 		services=new HashMap<>();
-		services.put("/controllerProject/member", new MemberService());
-		services.put("/controllerProject/board", new BoardService());
-		services.put("/controllerProject/unknown", new UnkownService());
-		
+		Enumeration<String> names=config.getInitParameterNames();
+		while(names.hasMoreElements()) {
+			String name=names.nextElement();
+			try {
+				Class cls=Class.forName(config.getInitParameter(name));//해당클레스를 메모리에 올림.
+				services.put(name,(ServiceController)(cls.newInstance()));
+			}catch(Exception e) {
+				
+			}
+		}
 
 	}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
