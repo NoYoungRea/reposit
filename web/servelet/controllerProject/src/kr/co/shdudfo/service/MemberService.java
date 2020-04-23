323e5b79bb39790acac93aaa3072b8f5fc635017
NoYoungRea <rea2169@naver.com>
2020-04-23 16:04:14 +0900

    controller4

--- web/servelet/controllerProject/src/kr/co/shdudfo/service/MemberService.java
+++ web/servelet/controllerProject/src/kr/co/shdudfo/service/MemberService.java
@@ -10,10 +10,35 @@ public class MemberService implements ServiceController{
 	@Override
 	public void doService(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.setContentType("text/html;charset=utf-8");
-		response.getWriter().println("게시판 서비스");	
-
+		String type=request.getParameter("type");
+		switch(type) {
+		case "insert" :
+			register(request,response);
+			break;
+		case "entire" :
+			entire(request,response);
+			break;
+		case "login" :
+			login(request,response);
+			break;
+		default:
+			;
+			
+		}
 		
 	}
+	private void register(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		response.getWriter().println("회원가입서비스");	
+	}
+	private void entire(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		response.getWriter().println("회원탈퇴서비스");	
 
+	}
+	private void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		response.getWriter().println("로그인 서비스");	
+
+	}
 }
